97d355fd0c6e2b624523cf52d6286845f3ea403f
rauanb <rauanb@hotmail.com>
2024-01-01 11:55:45 -0300

    adicionados ícones e padronização de telefone

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form-lista');
+let iconeTipo = "";
 const nomes = [];
 const numeros = [];
 const spanWhats = "ICONE Whats";
@@ -8,6 +9,7 @@ let linhas = '';
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+    selecionaTipo();
     adicionaLinha();
     atualizaTabela();
 })
@@ -16,8 +18,18 @@ function adicionaLinha() {
     const inputNome = document.getElementById('nome-contato');
     const inputNum = document.getElementById('numero-contato');
 
+    // Varifica se o contato já foi adicionado
     if (nomes.includes(inputNome.value)) {
-        alert(`O contato de ${inputNome.value} já foi adicionado!`);
+        // Identificar o número com o nome
+        let indNome = nomes.indexOf(inputNome.value);
+        let auxNum = numeros[indNome];
+        alert(`O contato de ${inputNome.value} já foi adicionado com o número ${auxNum}!`);
+        // Verifica se o número já foi adicionado
+    } else if (numeros.includes(inputNum.value)) {
+        // Identificar o nome com o número
+        let indNum = numeros.indexOf(inputNum.value);
+        let auxNom = nomes[indNum];
+        alert(`O número de ${inputNum.value} já foi adicionado no contade de ${auxNom}!`);
     } else {
         nomes.push(inputNome.value);
         numeros.push(inputNum.value);
@@ -25,6 +37,8 @@ function adicionaLinha() {
         let linha = '<tr>';
         linha += `<td>${inputNome.value}</td>`;
         linha += `<td>${inputNum.value}</td>`;
+        // linha += `<td>${iconeTipo}</td>`;
+        linha += `<td>${iconeTipo}</td>`;
         linha += `</tr>`;
         linhas = linha + linhas;
     }
@@ -36,4 +50,23 @@ function adicionaLinha() {
 function atualizaTabela() {
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas;
+}
+
+function selecionaTipo() {
+    let tipoContato = document.getElementsByName('tipo');
+
+    for (i = 0; i < tipoContato.length; i++) {
+        let auxTipo = "";
+        if (tipoContato[i].checked) {
+            auxTipo = tipoContato[i].id;
+            console.log(auxTipo);
+        }
+        
+        // Definição do ícone
+        if (auxTipo.includes("tel")) {
+            iconeTipo = `<img src="./img/telefone.png" alt="Ícone telefone" />`;
+        } else {
+            iconeTipo = `<img src="./img/whatsapp.png" alt="Ícone whatsapp" />`;
+        }
+    }
 }
